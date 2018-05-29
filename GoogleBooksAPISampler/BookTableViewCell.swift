dd8ae83eea18ae55edb901e5a3314d750305b30d
masuhara <info@masuhara.net>
2018-05-30 03:34:19 +0900

    Update cocoapods

---
+++ GoogleBooksAPISampler/BookTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BookTableViewCell.swift
+//  GoogleBooksAPISampler
+//
+//  Created by Masuhara on 2018/05/30.
+//  Copyright © 2018年 Ylab, Inc. All rights reserved.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
